22ee50d88be337974169a98bc5b572e56361d0c6
jafraessence <jafraessence@gmail.com>
2025-08-28 20:53:31 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ function contactarWhatsApp(producto) {
 }
 // script.js
 window.onload = function () {
-  alert("⚠️ Aun estoy trabajando en esta pagina pido su compresion, se espera que para el mes de septiembre la pagina este funcionando segun lo esperado, el unico apartado que funciona es el de perfumes de entrega rapidas es decir perfumes que tengo a la mano para vender. gracias por visitar la pagina");
+  alert("⚠️ BIENVENIDO A JAFRA ESSENCE, ESTA PAGINA AUN SE ENCUENTRA EN DESARROLO PLANEO HACERLE MEJORAS DE DISEÑOSY MAS, ESTA PAGINA ESTA DISEÑADA PARA MOSTRAR ESPECIALMENTE LOS PERFUMES DISPONIBLES PARA ENTREGAS RAPIDAS, ADEMAS ENCONTRARAS RECOMENDACION DE PRODUCTOS DE CATALAGO DEL MES.");
 };
